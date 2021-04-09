7e7ed85a20b6e5e0c27544ef3d1107e68d3ae27e
Jonathan <jonny@mondago.com>
2021-04-09 16:46:48 +0100

    Add dark mode

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,19 @@ export default function Home() {
   const [minutes, setMinutes] = useState(0);
   const pretty = HumanizeDuration(minutes * 60000);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50 dark:bg-gray-900">
       <Head>
         <title>Minutes Formatter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center dark:text-gray-100">
         <input
           onChange={(e) => setMinutes(e.target.value)}
           type="text"
           name="minutes"
           id="minutes"
-          className="px-2 rounded shadow border border-gray-300"
+          className="px-2 rounded shadow border border-gray-300 dark:border-gray-800 bg-gray-50 dark:bg-gray-800"
         />
         {pretty}
       </main>
